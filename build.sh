a221d942f04472a60738d8cd9e1eab991b1cd1f0
cornedev <corneproductions@gmail.com>
2026-04-21 18:52:32 +0200

    Change names.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 cd "$(dirname "$0")"
-python -m PyInstaller --onefile --windowed --icon=gfx/icon.ico --name ccplatformer game.py
+python -m PyInstaller --onefile --windowed --icon=gfx/terranox.ico --name terranox game.py
 cp -r gfx dist/gfx
 cp -r sfx dist/sfx
