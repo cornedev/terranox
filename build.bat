85bd5ef13e5bac2af9aaef619a57b11b8cdeb933
cornedev <corneproductions@gmail.com>
2026-04-21 18:52:18 +0200

    Change path to ico file and fix dll issue.

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ setlocal
 
 cd /d "%~dp0"
 
-python -m PyInstaller --onefile --windowed --icon=gfx\icon.ico --name ccplatformer game.py
+python -m PyInstaller --onefile --windowed --icon=gfx\terranox.ico --name terranox game.py
 
 xcopy gfx dist\gfx /E /I /Y
 xcopy sfx dist\sfx /E /I /Y
